549fd89f89e2fb22d3c9ee03a6265bbf7f12d756
thanhzanh <danhthanhvo23072003@gmail.com>
2024-10-02 09:09:23 +0700

    Cấu trúc thư mục

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+    return(
+        <footer>
+            <p>Copyright 2024 My React App</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Header() {
+    return(
+        <header>
+            <h1>Header React Vite</h1>
+        </header>
+    );
+}
+
+export default Header;
